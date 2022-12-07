c655ab552d43f8ef6c00812daf058b172310d926
DONGTRIEUPC\ADMIN <nineplusv01@gmail.com>
2022-12-07 16:13:22 +0700

    TuanNA add evidence before to custom clearance

--- src/main/java/com/nineplus/bestwork/repository/PackagePostRepository.java
+++ src/main/java/com/nineplus/bestwork/repository/PackagePostRepository.java
@@ -17,7 +17,7 @@ public interface PackagePostRepository extends JpaRepository<PackagePost, Long> 
 	
 	@Query(value = "SELECT aw.id as awbId, ft.post_package_id as postPackageId, ft.id as fileId, ft.type as type, ft.name as name , ft.path_file_server as pathFileServer "
 			+ "FROM FILE_STORAGE ft " + "JOIN PACKAGE_POST pp ON ft.post_package_id = pp.id "
-			+ "JOIN AIRWAY_BILL aw ON  aw.id = pp.airway_bill "
+			+ "JOIN AIRWAY_BILL aw ON aw.id = pp.airway_bill "
 			+ "WHERE aw.id = :awbId AND ft.is_choosen = 1", nativeQuery = true)
 	List<PackageFileProjection> getClearancePackageInfo(long awbId);
 
